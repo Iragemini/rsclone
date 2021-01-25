005b54dcd60636499b607d339a9bb96c5c1b0008
Iragemini <iragemini@gmail.com>
2021-01-26 00:44:31 +0300

    refactor: change titles

--- src/components/SelectArea.js
+++ src/components/SelectArea.js
@@ -9,7 +9,7 @@ const style = {
 function SelectArea(props) {
   return (
     <div className="container">
-      <select onChange={(event) => props.findArea(event.target.value)} className="form-control" style={style.select}>
+      <select onChange={(event) => props.findArea(event.target.value)} className="form-control" style={style.select} id="selectArea">
           <option></option>
           {
               props.areas.map((area, index) => <option key={index} value={area}>{area}</option>)
